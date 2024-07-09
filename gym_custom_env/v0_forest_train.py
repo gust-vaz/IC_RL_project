6a62f8a57b05437ae49f1165a31a720a52945e6c
gust-vaz <gustavo.vaz@usp.br>
2024-07-09 15:51:21 -0300

    first idea of train

--- gym_custom_env/v0_forest_train.py
+++ gym_custom_env/v0_forest_train.py
@@ -1,43 +1,39 @@
-import gymnasium as gym
 import numpy as np
-import matplotlib.pyplot as plt
-import random
-import pickle
-from stable_baselines3 import A2C
-import os
+import gymnasium as gym
+import mo_gymnasium as mo_gym
+from mo_gymnasium.utils import MORecordEpisodeStatistics
+from morl_baselines.multi_policy.pgmorl.pgmorl import PGMORL
+from morl_baselines.multi_policy.multi_policy_moqlearning.mp_mo_q_learning import MPMOQLearning
+# from morl_baselines.multi_policy.pareto_q_learning.pql import PQL
 import v0_forest_env
 
-def train_sb3():
-    model_dir = "models"
-    log_dir = "logs"
-    os.makedirs(model_dir, exist_ok=True)
-    os.makedirs(log_dir, exist_ok=True)
-
-    env = gym.make('forest-env-v0')
-    model = A2C('MlpPolicy', env, verbose=1)
-
-    TIMESTEPS = 1000
-    iters = 0
-    while iters < 10:
-        iters += 1
-        model.learn(total_timesteps=TIMESTEPS, reset_num_timesteps=False) # train
-        model.save(f"{model_dir}/a2c_{TIMESTEPS*iters}") # Save a trained model every TIMESTEPS
-
-def test_sb3(render=True):
-    env = gym.make('forest-env-v0', render_mode='human' if render else None)
-    model = A2C.load('models/a2c_2000', env=env)
-
-    # Run a test
-    obs = env.reset()[0]
-    terminated = False
-    while True:
-        action, _ = model.predict(observation=obs, deterministic=True)
-        obs, _, terminated, _, _ = env.step(action)
-
-        if terminated:
-            break
-
 if __name__ == '__main__':
-    # Train/test using StableBaseline3
-    train_sb3()
-    test_sb3()
+    # Train/test
+    GAMMA = 0.9
+    ref_point = np.array([-53000., -53000.])
+    env = mo_gym.make('forest-env-v0')
+    env = MORecordEpisodeStatistics(env, gamma=GAMMA)  # wrapper for recording statistics
+    eval_env = mo_gym.make('forest-env-v0') # environment used for evaluation
+
+    agent = PGMORL(
+        env_id='forest-env-v0',
+        origin=np.array([0.0, 0.0]),
+        gamma=GAMMA,
+        log=True,  # use weights and biases to see the results!
+    )
+    agent.train(total_timesteps=10000, eval_env=eval_env, ref_point=ref_point)
+    
+
+    # agent = MPMOQLearning(
+    #     env,
+    #     initial_epsilon=1.0,
+    #     final_epsilon=0.05,
+    #     epsilon_decay_steps=100000,
+    #     gamma=GAMMA,
+    #     dyna=True,
+    #     gpi_pd=True,
+    #     weight_selection_algo='gpi-ls',
+    #     use_gpi_policy=True
+    # )
+    # agent.train(total_timesteps=100000, timesteps_per_iteration=10000, eval_env=eval_env, num_eval_episodes_for_front=50, ref_point=ref_point)
+
